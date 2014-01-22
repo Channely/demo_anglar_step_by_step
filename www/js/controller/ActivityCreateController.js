5b1ac8deb76f2aaf136ed952e174ccfebb7fb108
Channely <qijie29896@gmail.com>
2014-01-22 11:10:09 +0800

    use $scope to bind a function to a button

--- www/js/controller/ActivityCreateController.js
+++ www/js/controller/ActivityCreateController.js
@@ -1,3 +1,7 @@
 function ActivityCreateController($scope){
     $scope.button_name = 'This is activity create page'
+
+    $scope.go_front_page = function(){
+        alert("I have no jumping code!!")
+    }
 }
