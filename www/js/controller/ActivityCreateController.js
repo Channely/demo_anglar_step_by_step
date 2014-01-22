74b4d76243dba34a045f993576d7c1633bb457e4
Channely <qijie29896@gmail.com>
2014-01-22 11:30:53 +0800

    change surface of button by using css-class-name

--- www/js/controller/ActivityCreateController.js
+++ www/js/controller/ActivityCreateController.js
@@ -2,7 +2,6 @@ function ActivityCreateController($scope, $navigate){
     $scope.button_name = 'This is activity create page'
 
     $scope.go_front_page = function(){
-        alert("I have jumping code!!")
         $navigate.go('/', 'slide', 'right')
     }
 }
