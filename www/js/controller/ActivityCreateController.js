a51c5a520c9f584383dda444fce6be57eb9e26d6
Channely <qijie29896@gmail.com>
2014-01-22 14:17:17 +0800

    use ng-change to check the input of price to display the sentence above real-time

--- www/js/controller/ActivityCreateController.js
+++ www/js/controller/ActivityCreateController.js
@@ -2,6 +2,10 @@ function ActivityCreateController($scope, $navigate){
     $scope.button_name = 'This is activity create page'
 
 
+    $scope.show_goods_sentence = function(){
+        $scope.goods_sentence = "Price of "+$scope.goods_name+" is $"+$scope.goods_price+"."
+    }
+
     $scope.save_goods_detail = function(){
         save_a_goods_info_to_localstorage($scope.goods_name, $scope.goods_price)
         $scope.go_front_page()
