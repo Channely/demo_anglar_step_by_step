1c1af2ba052a57e28dcd564b9ad39a18ded17039
Channely <qijie29896@gmail.com>
2014-01-22 13:45:32 +0800

    add two input components

--- www/js/controller/ActivityListController.js
+++ www/js/controller/ActivityListController.js
@@ -1,6 +1,6 @@
 function ActivityListController($scope, $navigate){
 
-    $scope.devices = [{name:'Goods',price:'Price'},{name:'Goods',price:'Price'},{name:'Goods',price:'Price'},{name:'Goods',price:'Price'}]
+    $scope.devices = [{name:'PC',price:'3600'},{name:'Mac',price:'3600'},{name:'Pad',price:'3600'},{name:'TV',price:'3600'},{name:'phone',price:'3600'},{name:'Player',price:'3600'}]
 
 
     $scope.go_next_page = function(){
