b3868ce100278b8ef1f8f9bcd5b532bd599708a6
Channely <qijie29896@gmail.com>
2014-01-22 11:38:07 +0800

    show the same result of normal_html vs $scope_bind

--- www/js/controller/ActivityListController.js
+++ www/js/controller/ActivityListController.js
@@ -1,5 +1,8 @@
 function ActivityListController($scope, $navigate){
 
+    $scope.name = 'Goods'
+    $scope.price = 'Price'
+
     $scope.go_next_page = function(){
         $navigate.go('/activity/create')
     }
