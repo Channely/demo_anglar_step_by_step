f2aaea4b1d8948c20e92fb5a59367759b601a056
Channely <qijie29896@gmail.com>
2014-01-22 11:55:53 +0800

    binding a array_form_data && showing it on page

--- www/js/controller/ActivityListController.js
+++ www/js/controller/ActivityListController.js
@@ -1,6 +1,6 @@
 function ActivityListController($scope, $navigate){
 
-    $scope.device = {name:'Goods',price:'Price'}
+    $scope.devices = [{name:'Goods',price:'Price'}]
 
 
     $scope.go_next_page = function(){
