9c5101c9b944aaf36072064d83f532f1d3951a6d
Channely <qijie29896@gmail.com>
2014-01-22 15:05:06 +0800

    reverse the goods list

--- www/js/controller/ActivityListController.js
+++ www/js/controller/ActivityListController.js
@@ -1,7 +1,7 @@
 function ActivityListController($scope, $navigate){
 
 //    $scope.devices = [{name:'PC',price:'3600'},{name:'Mac',price:'3600'},{name:'Pad',price:'3600'},{name:'TV',price:'3600'},{name:'phone',price:'3600'},{name:'Player',price:'3600'}]
-    $scope.devices = JSON.parse(localStorage.goods_detail||'[]')
+    $scope.devices = JSON.parse(localStorage.goods_detail||'[]').reverse()
 
     $scope.go_next_page = function(){
         $navigate.go('/activity/create')
